1759815f5b6264c25373b89ee4e66a4250b4362b
JoshFen95 <jfennell@companieshouse.gov.uk>
2021-02-15 15:26:08 +0000

    changes to remove deprecated class implementation

--- src/main/java/uk/gov/companieshouse/ocrapiconsumer/configuration/LoggingInterceptor.java
+++ src/main/java/uk/gov/companieshouse/ocrapiconsumer/configuration/LoggingInterceptor.java
@@ -1,35 +1,30 @@
 package uk.gov.companieshouse.ocrapiconsumer.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-import uk.gov.companieshouse.logging.Logger;
-import uk.gov.companieshouse.logging.util.RequestLogger;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
+import uk.gov.companieshouse.logging.util.RequestLogger;
+import uk.gov.companieshouse.ocrapiconsumer.OcrApiConsumerApplication;
+
 @Component
-public class LoggingInterceptor extends HandlerInterceptorAdapter implements RequestLogger {
-
-    @SuppressWarnings("squid:S1312") // Loggers should be private static final; here logger is singleton bean
-    private Logger logger;
-
-    @Autowired
-    public LoggingInterceptor(Logger logger) {
-        this.logger = logger;
-    }
-
+public class LoggingInterceptor implements HandlerInterceptor, RequestLogger {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OcrApiConsumerApplication.APPLICATION_NAME_SPACE);
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-       logStartRequestProcessing(request,logger);
-       return true;
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        logStartRequestProcessing(request, LOGGER);
+        return true;
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        logEndRequestProcessing(request, response, logger);
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           @Nullable ModelAndView modelAndView) {
+        logEndRequestProcessing(request, response, LOGGER);
     }
 }
