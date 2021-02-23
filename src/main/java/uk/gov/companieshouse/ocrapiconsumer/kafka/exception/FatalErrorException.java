95a05d3afa2c93c67f9e03b67c4f488435124150
SteveHicksCH <shicks@companieshouse.gov.uk>
2021-02-23 13:38:53 +0000

    use the attempt count in the ocr-request message to control count of retry messages

--- src/main/java/uk/gov/companieshouse/ocrapiconsumer/kafka/exception/FatalErrorException.java
+++ src/main/java/uk/gov/companieshouse/ocrapiconsumer/kafka/exception/FatalErrorException.java
@@ -1,11 +1,15 @@
 package uk.gov.companieshouse.ocrapiconsumer.kafka.exception;
 
+/**
+ * Call on potential coding bug or Kafka serious problem (e.g. can not sent message to retry topic) so we want
+ * to let Caller know that the ocr-request can not be made
+ */
 public class FatalErrorException extends RuntimeException {
     
     private static final long serialVersionUID = 1L;
 
-    public FatalErrorException(String message) {
-        super(message);
+    public FatalErrorException(String message, Throwable throwable) {
+        super(message, throwable);
     }
 
 }
