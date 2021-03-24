a6318faecd1559be4283948ef47d77edc4931f86
amyatt1CH <amyatt1@companieshouse.gov.uk>
2021-03-24 18:33:58 +0000

    Remove unused class and imports

--- src/test/java/uk/gov/companieshouse/ocrapiconsumer/IntegrationTest.java
+++ src/test/java/uk/gov/companieshouse/ocrapiconsumer/IntegrationTest.java
@@ -2,18 +2,13 @@ package uk.gov.companieshouse.ocrapiconsumer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestClientException;
 import uk.gov.companieshouse.ocrapiconsumer.groups.Integration;
 import uk.gov.companieshouse.ocrapiconsumer.request.TestParent;
 
